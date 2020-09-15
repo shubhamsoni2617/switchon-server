24cc8d884c4d3a6b0aa268282d6c87d7377b4e5c
Shubham.soni <Shubham.soni@ranosys.com>
2020-09-15 16:22:03 +0530

    Updated path

--- app/index.js
+++ app/index.js
@@ -7,12 +7,12 @@ const app = express();
 const port = process.env.PORT || 8000;
 app.use(express.json());
 app.use(cors());
-app.use(express.static("../switchon-client/build"));
+app.use(express.static("switchon-client/build"));
 
 app.use(require("./routes/user"));
 app.use(require("./routes/form"));
 app.use(function (req, res) {
-  res.sendFile(path.join(__dirname, "../../switchon-client/build/index.html"));
+  res.sendFile(path.join(__dirname, "../switchon-client/build/index.html"));
 });
 app.use(errorHandlers.notFound);
 app.use(errorHandlers.mongoseErrors);
